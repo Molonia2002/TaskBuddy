ecd1278b068b306fa979d0c364f39944ed077cc5
Molonia2002 <114032831+Molonia2002@users.noreply.github.com>
2025-07-20 06:51:38 +0100

    Update firebase.js
    Reset firebase.js file to conceal API keys

--- services/firebase.js
+++ services/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCSf_7fCW57-Mfi0d5uHBHsxtERyQkB5fU",
-  authDomain: "taskbuddy-d121c.firebaseapp.com",
-  projectId: "taskbuddy-d121c",
-  storageBucket: "taskbuddy-d121c.firebasestorage.app",
-  messagingSenderId: "966974947414",
-  appId: "1:966974947414:web:d7419d47328d423c5dbbf7",
-  measurementId: "G-VYR1M3RZJD"
+  apiKey: 
+  authDoma
+  projectId: 
+  storageBucket: 
+  messagingSenderId: 
+  appId: "1:
+  measurementId:
 };
 
 // Initialize Firebase
